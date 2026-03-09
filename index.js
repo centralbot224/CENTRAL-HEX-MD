33e2d450d803c13ba06c897da1984a72d84947b9
centralbot224 <centralbot224@gmail.com>
2026-03-09 14:01:12 +0000

    Update index.js

--- index.js
+++ index.js
@@ -160,7 +160,7 @@ async function startXeonBotInc() {
                             forwardingScore: 1,
                             isForwarded: true,
                             forwardedNewsletterMessageInfo: {
-                                newsletterJid: '20363408304719268@newsletter',
+                                newsletterJid: '120363408304719268@newsletter',
                                 newsletterName: 'CENTRAL-HEX-MD',
                                 serverMessageId: -1
                             }
@@ -269,7 +269,7 @@ async function startXeonBotInc() {
                         forwardingScore: 1,
                         isForwarded: true,
                         forwardedNewsletterMessageInfo: {
-                            newsletterJid: '20363408304719268@newsletter',
+                            newsletterJid: '120363408304719268@newsletter',
                             newsletterName: 'CENTRAL-HEX-MD',
                             serverMessageId: -1
                         }
