c38049a55ce501d3e4cb9ea82efcb372d86e258f
centralbot224 <centralbot224@gmail.com>
2026-03-09 13:35:40 +0000

    Update autostatus.js

--- commands/autostatus.js
+++ commands/autostatus.js
@@ -7,8 +7,8 @@ const channelInfo = {
         forwardingScore: 1,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363317041132843@newsletter',
-            newsletterName: 'CENTRAL-HEX',
+            newsletterJid: '20363408304719268@newsletter',
+            newsletterName: 'CENTRAL-HEX-MD',
             serverMessageId: -1
         }
     }
